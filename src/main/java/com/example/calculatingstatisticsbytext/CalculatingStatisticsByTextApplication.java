0261833aecf3ec2e8c8d1e3fe7f439b26ad23771
Artur Galimov <satorica@mail.ru>
2022-06-05 12:32:15 +0300

    initial

---
+++ src/main/java/com/example/calculatingstatisticsbytext/CalculatingStatisticsByTextApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculatingstatisticsbytext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatingStatisticsByTextApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CalculatingStatisticsByTextApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/calculatingstatisticsbytext/CalculatingStatisticsByTextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculatingstatisticsbytext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatingStatisticsByTextApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
